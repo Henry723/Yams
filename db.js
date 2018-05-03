fc88d81ca8578dbda307a6d75f8ac79b618430ac
Hayden <hyouk0902@gmail.com>
2018-05-02 22:24:08 -0700

    added inventory routing

---
+++ db.js
@@ -0,0 +1,14 @@
+﻿var express = require('express');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "sql3.freemysqlhosting.net",
+    user: "sql3235670",
+    password: "jdxZ7a9s55",
+    database: "sql3235670"
+});
+
+connection.connect();
+
+module.exports = mysql;
+module.exports = connection;
--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,49 @@
 var express = require('express');
 var router = express.Router();
-var foods = ["SPINACH", "CARROTS", "GREEN PEPPERS", "CELERY", "MUSHROOMS", "MIXED PEPPERS", "POTATOES"]
-
+var db = require('../db');
 
 /***login user and render dashboard****/
-router.post('/login', function(req, res, next){
-  console.log(req.body.email);
-  res.render('user');
+router.post('/login', function (req, res, next) {
+
+    db.query("SELECT ID FROM users WHERE email=" + JSON.stringify(req.body.email), function (error, result, field) {
+
+        if (error) {
+            console.log(error);
+        }
+        else {
+            var json = JSON.parse(JSON.stringify(result)); 
+            var userID = json[0].ID;
+
+            db.query("SELECT foodName, daysLeft FROM usersFoodData WHERE ID=" + userID, function (error, result, fields) {
+
+                res.render('user', JSON.parse(JSON.stringify(result)), function (err, html) {
+                    console.log("The user's food data sent");
+                    console.log(JSON.parse(JSON.stringify(result)));
+                 });
+            });
+        }
+    });
 });
 
 /***register user and render dashboard****/
-router.post('/register', function(req, res, next){
-  console.log(req.body.email);
-  res.render('user');
+router.post('/register', function (req, res, next) {
+    console.log(req.body.email);
+    res.render('user');
 });
 
 /****ajax response****/
-router.get('/allFoods', function(req, res, next) {
-  /**Pass through foodReference data***/
-  res.send(foods);
+router.get('/allFoods', function (req, res, next) {
+    /**Pass through foodReference data***/
+    res.send(foods);
 });
 
 /*******adding food to fridge****/
-router.post('/newFoodItem', function(req, res, next){
+router.post('/newFoodItem', function (req, res, next) {
 })
 
 /****delete item from fridge****/
-router.delete("/:foodID", function(req, res, next){
+router.delete("/:foodID", function (req, res, next) {
+
 })
 
 
