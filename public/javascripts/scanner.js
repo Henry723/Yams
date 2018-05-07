d7a98db5ca06f06a0babddb4b1af378d1207f054
Adam <Adamsmith6300@gmail.com>
2018-05-07 15:03:31 -0700

    fixed bug in scanner, no text in photos

--- public/javascripts/scanner.js
+++ public/javascripts/scanner.js
@@ -49,27 +49,32 @@ $( document ).ready(function() {
       dataType: 'json',
       contentType: 'application/json',
       success: function(data){
-        newReceiptFoodData = data.responses[0].fullTextAnnotation.text;
-        $.ajax({
-          type: "GET",
-          url: "/dashboard/allFoods",
-          success: function(data){
-            $("#close-loader").trigger("click");
-            var allFoods = data.foods;
-            var receiptDataText = newReceiptFoodData.toUpperCase();
-            $("#scannerForm form").empty();
-            var fData = "";
-            for (var key in allFoods){
-              var item = allFoods[key];
-              if (receiptDataText.includes(item.foodName.toUpperCase())){
-                // fData += "<tr><td>" + num + "</td><td>" + item.foodName.toUpperCase() +"</td><td>" + item.averageExpiryDate + "</td></tr>";
-                fData += "<div class='row'><div class='col'><input name='foodName' type='text' readonly class='form-control-plaintext' value='" + item.foodName.toUpperCase() + "' required></div><div class='col'><input name='expiryDate' type='number' class='form-control' value='" + item.averageExpiryDate + "' required></div></div>";
+        if ('fullTextAnnotation' in data.responses[0]){
+          newReceiptFoodData = data.responses[0].fullTextAnnotation.text;
+          $.ajax({
+            type: "GET",
+            url: "/dashboard/allFoods",
+            success: function(data){
+              $("#close-loader").trigger("click");
+              var allFoods = data.foods;
+              var receiptDataText = newReceiptFoodData.toUpperCase();
+              $("#scannerForm form").empty();
+              var fData = "";
+              for (var key in allFoods){
+                var item = allFoods[key];
+                if (receiptDataText.includes(item.foodName.toUpperCase())){
+                  // fData += "<tr><td>" + num + "</td><td>" + item.foodName.toUpperCase() +"</td><td>" + item.averageExpiryDate + "</td></tr>";
+                  fData += "<div class='row'><div class='col'><input name='foodName' type='text' readonly class='form-control-plaintext' value='" + item.foodName.toUpperCase() + "' required></div><div class='col'><input name='expiryDate' type='number' class='form-control' value='" + item.averageExpiryDate + "' required></div></div>";
+                }
               }
+              $("#scannerForm form").append(fData + "<button type='submit' class='btn'>Submit</button>");
+              $("#scannerForm").show();
             }
-            $("#scannerForm form").append(fData + "<button type='submit' class='btn'>Submit</button>");
-            $("#scannerForm").show();
-          }
-        });
+          });
+        } else {
+          $("#close-loader").trigger("click");
+          alert("No text in photo!");
+        }
       }
     });
 
