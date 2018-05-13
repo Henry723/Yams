a872d763cae325581b7e5b22db2d98947907b785
LAPTOP-G5629H2U\Jae Hyunhyo <jaehh1987@gmail.com>
2018-05-13 03:13:48 -0700

    add google register function move authentication related routers from fridge.js to home.js

--- config/auth.js
+++ config/auth.js
@@ -7,6 +7,10 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var db = require('./db');
 var Request = require('tedious').Request;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+const googleClientID = '921816998744-7bo3fu2ouecuhcdolbdj971af7p5pkrv.apps.googleusercontent.com';
+const googleClientSecret = 'TUjng4inPK-hmwaeoHQ1Dhz4';
 
 //used to serialize the user for the session
 passport.serializeUser(function(user, done) {
@@ -15,14 +19,17 @@ passport.serializeUser(function(user, done) {
 
 // used to deserialize the user
 passport.deserializeUser(function(email, done) {
+	console.log("Deserializing");
 	deserializeRequest =
 		new Request("select * from users where email = " + "'" + email + "'",
 		function(err, rowCount, row) {
+			console.log('rowCount: ', rowCount);
+			console.log('row: ', row);
+			console.log('row[0][0]: ', row[0][0].value);
+			console.log('row[0][1]: ', row[0][1].value);
 			var result = [];
 			result.email = row[0][0].value;
 			result.name = row[0][1].value;
-			result.password = row[0][2].value;
-			result.alarm = row[0][3].value;
 			done(err, result);
 		});
 	db.execSql(deserializeRequest);
@@ -54,19 +61,32 @@ passport.use(
 
 // configuration for google login.
 
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
-const googleClientID = '921816998744-7bo3fu2ouecuhcdolbdj971af7p5pkrv.apps.googleusercontent.com';
-const googleClientSecret = 'TUjng4inPK-hmwaeoHQ1Dhz4';
-
 passport.use(new GoogleStrategy({
-    clientID: googleClientID,
+	clientID: googleClientID,
     clientSecret: googleClientSecret,
-    callbackURL: "http://localhost:3000/login/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-	  return done(null, profile.emails[0].value);
-  }
-));
+    callbackURL: "http://localhost:3000/auth/google/callback"
+	},
+	function(accessToken, refreshToken, profile, done) {
+			var email = profile.emails[0].value;
+			loginRequest = new Request("select * from users where email = '" + email + "'", function(err, rowCount, row) 
+			{
+					
+				if (err) { return done(err); }
+					
+				if (!rowCount) 
+				{
+					registerRequest = new Request("INSERT INTO users (name, email) VALUES ('"
+							+ profile.displayName + "', '" + email + "')", 
+							function (err, rowCount, rows) {return done(null, email);
+					});
+					db.execSql(registerRequest);
+				} 
+				else 
+				{
+					return done(null, email);
+				}
+			});
+			db.execSql(loginRequest);		
+	}));
 
 module.exports = passport;
--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../config/db');
-var passport = require("../config/authLocal");
+var passport = require("../config/auth");
 var Request = require('tedious').Request;
 
 /* GET Home page. */
@@ -9,11 +9,42 @@ router.get('/', function(req, res, next) {
 	res.render('home', { message: req.flash('error')});
 });
 
-router.get('/login', 
+/***login user and render dashboard****/
+router.post('/auth/local/login',
+		passport.authenticate('local-login', {
+			successRedirect: '/fridge/getUserFoodData',
+			failureRedirect: '/',
+			failureFlash: true
+}));
+
+/***register user and render dashboard****/
+router.post('/auth/local/register', function (req, res, next) {
+
+    request = new Request("INSERT INTO users (name, email, password) VALUES ('" +
+        req.body.name + "', '" + req.body.email + "', '" + req.body.password + "')",
+        function (error)
+        {
+            if (error)
+            {
+                console.log(error.message);
+                throw error;
+            }
+            else
+            {
+                console.log("login success");
+                res.redirect("/");
+            }
+        }
+    );
+    db.execSql(request);
+
+});
+
+router.get('/auth/google', 
 		passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.email'] }));
 
 
-router.get('/login/callback',
+router.get('/auth/google/callback',
 		passport.authenticate('google', { failureRedirect: '/' }),
 		function(req, res) {
 			console.log('call back!');
