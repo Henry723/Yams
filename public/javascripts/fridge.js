b97a54c6e025f781cdabb8ad933598c99f87c980
Henry Zhang <henryisgood@live.com>
2018-05-15 19:07:08 -0700

    Woot animation on navbar?

--- public/javascripts/fridge.js
+++ public/javascripts/fridge.js
@@ -88,6 +88,26 @@ $( document ).ready(function() {
             }
         });
     });
+    
+  /**********************header timer***********************/
+    
+    var didScroll;
+    // on scroll, let the interval function know the user has scrolled
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+    // run hasScrolled() and reset didScroll status
+    setInterval(function() {
+        if (didScroll) {
+            $("#headerNav").slideDown(500);
+            didScroll = false;
+        }
+    }, 250);
+    
+    setInterval(function() {
+        $("#headerNav").slideUp(500)
+    }, 6000);
+
 });
 
 
